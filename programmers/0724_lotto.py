36b6c25d109a468d48c98fffe12b905726a878ea
julie0919 <julie.lee0919@gmail.com>
2021-08-01 23:26:04 +0900

    algorithm

--- programmers/0724_lotto.py
+++ programmers/0724_lotto.py
@@ -1,32 +1,26 @@
 def solution(lottos, win_nums):
     count = 0
-    count_zero = 0
-    max_rank = 0
-    min_rank = 0
-
-    for num in lottos:
-        if num == 0:
-            count_zero += 1
-
-        for win_num in win_nums:
-            if num == win_num:
-                count += 1
-        
-    total_count = count + count_zero    
-
-    if total_count == 2 or total_count == 1:
-        max_rank = 5
-    elif total_count == 0:
-        max_rank = 6
-    else:
-        max_rank = 7 - total_count
+    count_zero = lottos.count(0)
     
-    if count == 2 or count == 1:
-        min_rank = 5
-    elif count == 0:
-        min_rank = 6
+    for num in lottos:
+        for win_num in win_nums:
+            if num != 0 and num == win_num:
+                count += 1
+
+    if count_zero == 6:
+        return [1, 6]
+    elif count_zero == 0 and count == 0:
+        return [6, 6]
+        
+    min_count = count
+    max_count = count + count_zero
+
+    max_rank = 7 - max_count
+    
+    if min_count > 0:
+        min_rank = 7 - min_count
     else:
-        min_rank = 7 - total_count
+        min_rank = 6
 
     answer = [max_rank, min_rank]
     return answer
